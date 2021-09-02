8bcf488102639f0c37f09951b7707c2e3553ddcc
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-02 20:06:34 +0900

    icon 추가

--- src/components/FindPassword.js
+++ src/components/FindPassword.js
@@ -1,17 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FaUserLock } from "react-icons/fa";
 
 const FindPassword = () => {
-  const stringData =
-    "이메일 주소를 입력하시면 비밀번호를 변경할 수 있는 링크를 <br/> 보내드립니다.";
   return (
     <>
       <div>
         <div id="form">
-          <div class="form-logo__find">
+          <div className="form-logo__find">
             <h3>Eatstagram</h3>
             <h1>
-              <i class="fas fa-user-lock"></i>
+              <FaUserLock />
             </h1>
             <h2>비밀 번호를 잊으셨나요?</h2>
             <h4>이메일 주소를 입력하시면 비밀번호를 변경할 수 있는 링크를</h4>
@@ -20,7 +19,7 @@ const FindPassword = () => {
           <form action="main.html" method="GET" id="find-form">
             <input name="usermail" type="text" placeholder="이메일 주소" />
             <input type="submit" value="비밀번호 변경 링크 보내기" />
-            <div class="line">
+            <div className="line">
               <span>또는</span>
             </div>
             <Link to="/Join">
@@ -28,16 +27,16 @@ const FindPassword = () => {
             </Link>
           </form>
         </div>
-        <div class="form-bottom__btn">
+        <div className="form-bottom__btn">
           <Link to="/">
-            <h2 class="form-bottom__link">로그인</h2>
+            <h2 className="form-bottom__link">로그인</h2>
           </Link>
           <h2>으로 돌아가기</h2>
         </div>
       </div>
       <div id="footer">
         <div>
-          <ul class="footer-top">
+          <ul className="footer-top">
             <li>소개</li>
             <li>블로그</li>
             <li>채용 정보</li>
@@ -50,7 +49,7 @@ const FindPassword = () => {
             <li>위치</li>
           </ul>
         </div>
-        <div class="footer-bottom">
+        <div className="footer-bottom">
           <p>© 2021 Eatstagram from JinYedo ParkSangJun BaeGyuri</p>
         </div>
       </div>
