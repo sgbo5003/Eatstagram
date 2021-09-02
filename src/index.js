ea10f753085879ae4b5f999c43219f457269c6b7
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-03 00:53:07 +0900

    axios 설정
    baseUrl 세팅

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "../src/css/style.css";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.baseURL = "http://localhost:8080/";
 
 ReactDOM.render(
   <BrowserRouter>
