022330fe0458ba749a627d2960e9fd2fa750a1bb
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-03 00:53:24 +0900

    JoinEail 컴포넌트 추가
    JoinEail 컴포넌트 추가

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Route, Switch } from "react-router";
 import FindPassword from "./components/FindPassword";
 import Join from "./components/Join";
+import JoinEmail from "./components/JoinEmail";
 import Login from "./components/Login";
 
 const Router = () => {
@@ -10,6 +11,7 @@ const Router = () => {
       <Route exact path="/" component={Login} />
       <Route path="/Join" component={Join} />
       <Route path="/FindPassword" component={FindPassword} />
+      <Route path="/JoinEmail" component={JoinEmail} />
     </Switch>
   );
 };
