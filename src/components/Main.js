634179281ddd81305636fb2927ad30a323a441c5
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-06 00:54:29 +0900

    비디오 스크롤 이벤트 제어
    useEffect를 사용한 스크롤 이벤트 제어
    스크롤 할 때마다 화면 높이에 따른 동영상 재생

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,56 +1,48 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Main = () => {
-  const videoStartPoint = window.innerHeight / 2.5; // 비디오 시작 지점
-  const videoIds = [];
-  const ref = useRef();
+  useEffect(() => {
+    const videoStartPoint = window.innerHeight / 2.5; // 비디오 시작 지점
+    const videoIds = [];
 
-  // 재생시킬 <video> 태그의 id 값들을 저장하기 위한 반복문
-  for (let i = 1; i <= document.getElementsByTagName("video").length; i++) {
-    // .wrap의 자식요소(<video> 태그)만큼 반복
-    videoIds.push(
-      document
-        .querySelector(".wrap video:nth-child(" + i + ")")
-        .getAttribute("id")
-    ); // 반복 요소의 id값을 배열에 저장
-  }
+    // 재생시킬 <video> 태그의 id 값들을 저장하기 위한 반복문
+    for (let i = 1; i <= document.getElementsByTagName("video").length; i++) {
+      // .wrap의 자식요소(<video> 태그)만큼 반복
+      videoIds.push(
+        document
+          .querySelector(".wrap video:nth-child(" + i + ")")
+          .getAttribute("id")
+      ); // 반복 요소의 id값을 배열에 저장
+    }
 
-  document.addEventListener("scroll", function () {
-    console.log("dsafdsa");
-    // 스크롤시 이벤트 발생
-    videoIds.forEach((element) => {
-      // 배열의 개수(<video> 태그의 수) 만큼 반복
-      const elementTop = document
-        .querySelector("#" + element)
-        .getBoundingClientRect().top; // 반복 요소(<video> 태그)의 현재 위치값
-      if (
-        elementTop < videoStartPoint &&
-        elementTop > window.innerHeight / 100
-      ) {
-        document.querySelector("#" + element).play();
-      } else if (elementTop > videoStartPoint || elementTop < 0) {
-        document.querySelector("#" + element).pause();
-      }
+    document.addEventListener("scroll", function () {
+      // 스크롤시 이벤트 발생
+      videoIds.forEach((element) => {
+        // 배열의 개수(<video> 태그의 수) 만큼 반복
+        const elementTop = document
+          .querySelector("#" + element)
+          .getBoundingClientRect().top; // 반복 요소(<video> 태그)의 현재 위치값
+
+        if (
+          elementTop < videoStartPoint &&
+          elementTop > window.innerHeight / 100
+        ) {
+          document.querySelector("#" + element).play();
+        } else if (elementTop > videoStartPoint || elementTop < 0) {
+          document.querySelector("#" + element).pause();
+        }
+      });
     });
-  });
-
-  console.log(ref);
-
-  const onClickHandler = (e) => {
-    console.log(e);
-  };
+  }, []);
 
   return (
     <div className="wrap">
-      <div>배열: {videoIds.length}</div>
       <video
         controls
         muted="muted"
         id="test1"
         width="500"
         style={{ margin: "40px" }}
-        onClick={onClickHandler}
-        ref={ref}
       >
         <source
           src="http://localhost:8080/stream/왜그래.mp4"
