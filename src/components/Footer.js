cfe30fbc4e822097437b00ef77e5101da2701fd7
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-06 01:08:53 +0900

    Footer 컴포넌트화

---
+++ src/components/Footer.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div id="footer">
+      <div>
+        <ul className="footer-top">
+          <li>소개</li>
+          <li>블로그</li>
+          <li>채용 정보</li>
+          <li>도움말</li>
+          <li>API</li>
+          <li>개인정보처리방침</li>
+          <li>약관</li>
+          <li>인기 계정</li>
+          <li>해시태그</li>
+          <li>위치</li>
+        </ul>
+      </div>
+      <div className="footer-bottom">
+        <p>© 2021 Eatstagram from JinYedo ParkSangJun BaeGyuri</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
