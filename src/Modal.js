691cd05e0ecfc02b5e74ab38c2111198a6b85564
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-10 04:03:38 +0900

    모달 JS 생성

---
+++ src/Modal.js
@@ -0,0 +1,47 @@
+import React, {
+  useEffect,
+  useImperativeHandle,
+  useState,
+  forwardRef,
+  useCallback,
+} from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children, fade = false, isOpen, setIsOpen }, ref) => {
+  const close = useCallback(() => setIsOpen(false), []);
+
+  useImperativeHandle(
+    ref,
+    () => ({
+      open: () => setIsOpen(true),
+      close,
+    }),
+    [close]
+  );
+
+  const handleEscape = useCallback(
+    (event) => {
+      if (event.keyCode === 27) close();
+    },
+    [close]
+  );
+
+  useEffect(() => {
+    if (isOpen) document.addEventListener("keydown", handleEscape, false);
+    return () => {
+      document.removeEventListener("keydown", handleEscape, false);
+    };
+  }, [handleEscape, isOpen]);
+
+  return createPortal(
+    isOpen ? (
+      <div className="ly_popover">
+        <div className="ly_popover-overlay" onClick={close} />
+        <div>{children}</div>
+      </div>
+    ) : null,
+    document.getElementById("modal_root")
+  );
+};
+
+export default forwardRef(Modal);
