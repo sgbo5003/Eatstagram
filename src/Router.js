c8fc4188b2ae5d2990b5280cb800fea7605c33a6
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-10 04:04:05 +0900

    새로운 컴포넌트들 추가

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { Route, Switch } from "react-router";
 import FindPassword from "./components/FindPassword";
+import Home from "./components/Home";
 import Join from "./components/Join";
 import JoinEmail from "./components/JoinEmail";
 import Login from "./components/Login";
 import Main from "./components/Main";
+import WriteModal from "./components/WriteModal";
 
 const Router = () => {
   return (
@@ -13,7 +15,9 @@ const Router = () => {
       <Route path="/Join" component={Join} />
       <Route path="/FindPassword" component={FindPassword} />
       <Route path="/JoinEmail" component={JoinEmail} />
+      <Route path="/Home" component={Home} />
       <Route path="/Main" component={Main} />
+      <Route path="/WriteModal" component={WriteModal} />
     </Switch>
   );
 };
