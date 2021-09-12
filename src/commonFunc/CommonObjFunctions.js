d25ff1bd60bf0e63cbd57de87bd7f96caa863267
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-13 01:37:47 +0900

    axios 공용함수 생성 & 수정
    axios 공용함수 생성 & 수정

--- src/commonFunc/CommonObjFunctions.js
+++ src/commonFunc/CommonObjFunctions.js
@@ -14,25 +14,8 @@ export const executeQuery = ({ url, data, success, error, fail }) => {
     data: params || {},
   })
     .then((res) => {
-      if (res.data.response === "error") {
-        if (error) {
-          error(res.data);
-          console.log("error1", res);
-        } else {
-          alert(res.data.msg);
-          console.log("error2", res);
-        }
-      }
-      if (res.data.response === "fail") {
-        // alert("서버접속에 실패하였습니다. 관리자에게 문의해주시기 바랍니다.");
-        fail(res);
-        console.log("fail1", res);
-        console.log(res.data);
-      }
-      if (res.data.response === "ok") {
-        console.log(res);
-        success(res.data);
-      }
+      console.log(res);
+      success(res.data);
     })
     .catch((err) => {
       if (fail) {
