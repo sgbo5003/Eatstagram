509b0029b8257a0fd1afbea5d555f339588e0970
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-13 01:49:08 +0900

    드롭다운 관련
    - 카카오맵과 연동하여 드롭다운에 리스트 뿌려주는 기능 구현

--- src/components/Qmenu.js
+++ src/components/Qmenu.js
@@ -1,38 +1,30 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
 import tailImg from "../images/tail.png";
 
 const Qmenu = (props) => {
-  const qmenuList = [
-    {
-      link: "/AccountManagement",
-      text: "계정관리",
-    },
-    {
-      link: "/ChangePassword",
-      text: "비밀번호 변경",
-    },
-    {
-      link: "/Inquiry",
-      text: "문의하기",
-    },
-    {
-      link: "/Notice?page=1",
-      text: "공지사항",
-    },
-  ];
+  const { locationList, setUserLocation, setIsDropClick } = props;
+
+  // 위치 리스트를 클릭 했을 때
+  const onListClickHandler = (data) => {
+    setUserLocation(data.place_name);
+    setIsDropClick(false);
+  };
   return (
-    <div className="Qmenu_bar_on">
-      <span className="tail_on">
-        <img className="tail_img" src={tailImg} alt="tail.png" />
-      </span>
-      {qmenuList.map((data, index) => {
-        return (
-          <p className="Qmenu_menu_cove" key={index}>
-            <li className="Qmenu_menu">{data.text}</li>
-          </p>
-        );
-      })}
+    <div className="map-dropdown">
+      <img src={tailImg} alt="" />
+      <div className="map-dropdown-window">
+        {locationList.map((data, idx) => {
+          return (
+            <div
+              className="map-dropdown-place"
+              key={idx}
+              onClick={() => onListClickHandler(data)}
+            >
+              <h1>{data.place_name}</h1>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
