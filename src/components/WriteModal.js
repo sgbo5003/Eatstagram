5cf33071efceb6a42952b1e2a70cc736c64d92fd
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-13 01:51:15 +0900

    글쓰기 모달창 관련
    - 위치 입력 시 드롭다운 추가, 카카오맵 불러오는 기능 추가
    - 닫기 모달 불러오는 기능 추가

--- src/components/WriteModal.js
+++ src/components/WriteModal.js
@@ -11,6 +11,9 @@ import myProfileImg from "../images/묭수.jpg";
 import WriteDefaultImg from "../images/write_default_image.png";
 import axios from "axios";
 import Qmenu from "./Qmenu";
+import * as fnc from "../commonFunc/CommonFunctions";
+import Modal from "../Modal";
+import WriteExitConfirmModal from "./WriteExitConfirmModal";
 
 const WriteModal = () => {
   const category = [
@@ -27,14 +30,15 @@ const WriteModal = () => {
 
   const [content, setContent] = useState(""); // 글작성 - 글쓰기 부분
   const [userLocation, setUserLocation] = useState(""); // 위치
+  const [locationList, setLocationList] = useState([]); // 카카오 맵 위치 data list
   const [inputHash, setInputHash] = useState(""); // 글작성 - 해쉬태그 input
   const [hashList, setHashList] = useState([]); // 해쉬태그를 담는 배열
   const [categoryList, setCategoryList] = useState(new Set()); // 카테고리
   const [userImg, setUserImg] = useState([]); // 유저가 올린 이미지(하단)
-  const [userImgList, setUserImgList] = useState([]); // 유저가 올린 이미지(상단)
+  const [userImgList, setUserImgList] = useState([]); // 유저가 올린 이미지 & 동영상(상단)
   const [videoImg, setVideoImg] = useState([]); // 유저가 올린 동영상(하단)
-  //   const [userVideoList, setUserVideoList] = useState([]); // 유저가 올린 동영상(상단)
   const [fileImg, setFileImg] = useState([]); // 파일 업로드 할 이미지
+  const [modalOn, setModalOn] = useState(false); // 글 작성창 -> x 표시 클릭 -> 확인하는 모달창 on
   const [isDropClick, setIsDropClick] = useState(false); // Qmenu ON & OFF
 
   // 글작성 textarea부분
@@ -106,6 +110,7 @@ const WriteModal = () => {
       };
       reader.readAsDataURL(e.target.files[0]);
     }
+    console.log(fileImg);
   };
 
   // 하단 이미지 선택 시
@@ -125,6 +130,7 @@ const WriteModal = () => {
       .then((response) => {
         console.log(response);
         console.log(response.data.documents); // 검색결과 []
+        setLocationList(response.data.documents);
       })
       .catch((error) => {
         console.log(error);
@@ -133,33 +139,37 @@ const WriteModal = () => {
 
   const onLocationClick = () => {
     kakaoMap();
-    // setIsDropClick(true);
+    setIsDropClick(true);
   };
 
   const onSubmit = (e) => {
-    e.preventDefault();
     const categoryArray = [...categoryList];
-    const params = new FormData();
-    params.append("text", content); // 글쓰기 text
-    params.append("location", userLocation); // 위치
-    params.append("username", "1890067550"); // 유저 아이디
+    const paramsData = new FormData();
+    paramsData.append("text", content); // 글쓰기 text
+    paramsData.append("location", userLocation); // 위치
+    paramsData.append("username", sessionStorage.getItem("username")); // 유저 아이디
     for (let i = 0; i < hashList.length; i++) {
-      params.append(`contentHashtagDTOList[${i}].hashtag`, hashList[i]);
+      paramsData.append(`contentHashtagDTOList[${i}].hashtag`, hashList[i]);
     } // 해쉬태그
     for (let i = 0; i < categoryArray.length; i++) {
-      params.append(`contentCategoryDTOList[${i}].category`, categoryArray[i]);
+      paramsData.append(
+        `contentCategoryDTOList[${i}].category`,
+        categoryArray[i]
+      );
     } // 카테고리
     for (let i = 0; i < fileImg.length; i++) {
-      params.append("uploadFiles", fileImg[i]);
+      paramsData.append("uploadFiles", fileImg[i]);
     } // 이미지 & 동영상 파일
+
     axios({
       method: "post",
       url: "content/add",
-      data: params,
+      data: paramsData,
     })
       .then((response) => {
         if (response.data.response == "ok") {
           alert("글쓰기 성공");
+          location.reload();
         } else {
           return;
         }
@@ -168,172 +178,194 @@ const WriteModal = () => {
         console.log(error);
         console.log("hi");
       });
+    // fnc.executeQuery({
+    //   url: "content/add",
+    //   data: { paramsData },
+    //   success: (res) => {
+    //     alert("글 작성 성공");
+    //   },
+    //   fail: (res) => {
+    //     alert("글 작성 실패");
+    //   },
+    // });
+  };
+
+  const onExitButtonHandler = () => {
+    setModalOn(true);
   };
 
   return (
-    <div className="write-window">
-      <div className="write-area">
-        <div className="wirte-area__top">
-          <p>작성</p>
-          <button>
-            <FaTimes />
-          </button>
-        </div>
-        <div className="write-area__bottom">
-          <div className="write-left">
-            <div className="write-contents">
-              <div className="write-contnets__main">
-                {fileImg.length === 0 ? (
-                  <img src={WriteDefaultImg} alt="기본 이미지" />
-                ) : (
-                  userImgList.map((data, idx) => {
-                    if (data.type === "image/png") {
-                      return <img src={data.result} alt="이미지" key={idx} />;
-                    } else if (data.type === "video/mp4") {
-                      return (
-                        <video controls width="500" height="500">
-                          <source
-                            src={data.result}
-                            type="video/mp4"
-                            key={idx}
-                          />
-                        </video>
-                      );
-                    } else {
-                      return;
-                    }
-                  })
-                )}
-              </div>
-            </div>
-            <div className="write-upload">
-              {userImg.length === 0
-                ? ""
-                : userImg.map((data, idx) => {
-                    return (
-                      <div
-                        className="write-upload__plus"
-                        onClick={() => onImgClickHandler(data)}
-                        key={idx}
-                      >
-                        <img src={data.result} alt="이미지" />
-                      </div>
-                    );
-                  })}
-              {videoImg.length === 0
-                ? ""
-                : videoImg.map((data, idx) => {
-                    return (
-                      <div
-                        className="write-upload__plus"
-                        onClick={() => onImgClickHandler(data)}
-                        key={idx}
-                      >
-                        <video controls>
-                          <source src={data.result} type="video/mp4" />
-                        </video>
-                      </div>
-                    );
-                  })}
-              <div className="write-upload__plus">
-                <input
-                  type="file"
-                  id="getfile"
-                  accept="image/*, video/*"
-                  onChange={onChangeImg}
-                  multiple
-                />
-                <label htmlFor="getfile">
-                  <BsPlus className="write-upload-plus-icon" />
-                </label>
-              </div>
-            </div>
+    <>
+      <div className="write-window">
+        <div className="write-area">
+          <div className="wirte-area__top">
+            <p>작성</p>
+            <button>
+              <FaTimes onClick={onExitButtonHandler} />
+            </button>
           </div>
-          <div className="write-right">
-            <div className="write-user">
-              <div className="write-user__img">
-                <img src={myProfileImg} alt="" />
+          <div className="write-area__bottom">
+            <div className="write-left">
+              <div className="write-contents">
+                <div className="write-contnets__main">
+                  {fileImg.length === 0 ? (
+                    <img src={WriteDefaultImg} alt="기본 이미지" />
+                  ) : (
+                    userImgList.map((data, idx) => {
+                      if (data.type === "image/png") {
+                        return <img src={data.result} alt="이미지" key={idx} />;
+                      } else if (data.type === "video/mp4") {
+                        return (
+                          <video controls width="500" height="500" key={idx}>
+                            <source src={data.result} type="video/mp4" />
+                          </video>
+                        );
+                      } else {
+                        return;
+                      }
+                    })
+                  )}
+                </div>
               </div>
-              <div className="write-user__id">
-                <h1>gyuxxr</h1>
+              <div className="write-upload">
+                {userImg.length === 0
+                  ? ""
+                  : userImg.map((data, idx) => {
+                      return (
+                        <div
+                          className="write-upload__plus"
+                          onClick={() => onImgClickHandler(data)}
+                          key={idx}
+                        >
+                          <img src={data.result} alt="이미지" />
+                        </div>
+                      );
+                    })}
+                {videoImg.length === 0
+                  ? ""
+                  : videoImg.map((data, idx) => {
+                      return (
+                        <div
+                          className="write-upload__plus"
+                          onClick={() => onImgClickHandler(data)}
+                          key={idx}
+                        >
+                          <video controls>
+                            <source src={data.result} type="video/mp4" />
+                          </video>
+                        </div>
+                      );
+                    })}
+                <div className="write-upload__plus">
+                  <input
+                    type="file"
+                    id="getfile"
+                    accept="image/*, video/*"
+                    onChange={onChangeImg}
+                    multiple
+                  />
+                  <label htmlFor="getfile">
+                    <BsPlus className="write-upload-plus-icon" />
+                  </label>
+                </div>
               </div>
             </div>
-            <div className="write-text">
-              <textarea
-                name=""
-                cols="100"
-                rows="100"
-                placeholder="설명을 입력하세요.."
-                value={content}
-                onChange={onContentHandler}
-              />
-            </div>
-            <div className="write-item">
-              <div className="write-map">
-                <input
-                  type="text"
-                  placeholder="위치 추가..."
-                  value={userLocation}
-                  onChange={onUserLocationHandler}
+            <div className="write-right">
+              <div className="write-user">
+                <div className="write-user__img">
+                  <img src={myProfileImg} alt="" />
+                </div>
+                <div className="write-user__id">
+                  <h1>gyuxxr</h1>
+                </div>
+              </div>
+              <div className="write-text">
+                <textarea
+                  name=""
+                  cols="100"
+                  rows="100"
+                  placeholder="설명을 입력하세요.."
+                  value={content}
+                  onChange={onContentHandler}
                 />
-                {isDropClick && <Qmenu />}
-                <button onClick={onLocationClick}>
-                  <FaMapMarkerAlt />
-                </button>
               </div>
-              <div className="write-hashtag">
-                <h1>해시태그</h1>
-                <div className="write-hashtag__input">
+              <div className="write-item">
+                <div className="write-map">
                   <input
                     type="text"
-                    value={inputHash}
-                    onChange={onInputHashHandler}
+                    placeholder="위치 추가..."
+                    value={userLocation}
+                    onChange={onUserLocationHandler}
                   />
-                  <input
-                    type="button"
-                    value="추가"
-                    onClick={onHashAddHandler}
-                  />
-                  <FaHashtag className="write-hashtag-icon" />
-                  <div className="write-hashtag-hashlist">
-                    {hashList.map((data, idx) => {
+                  {isDropClick && (
+                    <Qmenu
+                      locationList={locationList}
+                      setLocationList={setLocationList}
+                      setUserLocation={setUserLocation}
+                      setIsDropClick={setIsDropClick}
+                    />
+                  )}
+                  <button onClick={onLocationClick}>
+                    <FaMapMarkerAlt />
+                  </button>
+                </div>
+                <div className="write-hashtag">
+                  <h1>해시태그</h1>
+                  <div className="write-hashtag__input">
+                    <input
+                      type="text"
+                      value={inputHash}
+                      onChange={onInputHashHandler}
+                    />
+                    <input
+                      type="button"
+                      value="추가"
+                      onClick={onHashAddHandler}
+                    />
+                    <FaHashtag className="write-hashtag-icon" />
+                    <div className="write-hashtag-hashlist">
+                      {hashList.map((data, idx) => {
+                        return (
+                          <div className="write-hashtag__into" key={idx}>
+                            <h4># {data}</h4>
+                            <button onClick={() => onHashDeleteHandler(data)}>
+                              X
+                            </button>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
+                </div>
+                <div className="write-category">
+                  <h1>카테고리</h1>
+                  <div id="food-category">
+                    {category.map((data, idx) => {
                       return (
-                        <div className="write-hashtag__into" key={idx}>
-                          <h4># {data}</h4>
-                          <button onClick={() => onHashDeleteHandler(data)}>
-                            X
+                        <div className="category-row" key={idx}>
+                          <button
+                            className={categoryList.has(data) ? "selected" : ""}
+                            onClick={() => onCategoryListHandler(data)}
+                          >
+                            {data}
                           </button>
                         </div>
                       );
                     })}
                   </div>
                 </div>
-              </div>
-              <div className="write-category">
-                <h1>카테고리</h1>
-                <div id="food-category">
-                  {category.map((data, idx) => {
-                    return (
-                      <div className="category-row" key={idx}>
-                        <button
-                          className={categoryList.has(data) ? "selected" : ""}
-                          onClick={() => onCategoryListHandler(data)}
-                        >
-                          {data}
-                        </button>
-                      </div>
-                    );
-                  })}
+                <div className="write-btn">
+                  <button onClick={onSubmit}>공유하기</button>
                 </div>
-              </div>
-              <div className="write-btn">
-                <button onClick={onSubmit}>공유하기</button>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      <Modal isOpen={modalOn} setIsOpen={setModalOn}>
+        <WriteExitConfirmModal setModalOn={setModalOn} />
+      </Modal>
+    </>
   );
 };
 
