ae2d0598d4befafed25ad9f2f0922e31b8c680f9
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-13 01:43:27 +0900

    헤더 아이콘 변경
    헤더 아이콘 변경

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   FaHome,
   FaPaperPlane,
@@ -7,36 +7,53 @@ import {
   FaRegCompass,
   FaHeart,
   FaRegHeart,
-} from "react-icons/fa";
+  FaPlusSquare,
+  FaRegPlusSquare,
+} from "react-icons/fa"; // Reg 붙은게 색깔 없는거
 import myProfileImg from "../images/묭수.jpg";
-
+import Modal from "../Modal";
+import WriteModal from "./WriteModal";
 const Header = () => {
-  return (
-    <div className="header">
-      <div className="header-area">
-        <div>
-          <h1>Eatstagram</h1>
-        </div>
+  const [writeModalOn, setWriteModalOn] = useState(false);
 
-        <div className="header-area__icons">
-          <p>
-            <FaHome />
-          </p>
-          <p>
-            <FaRegPaperPlane />
-          </p>
-          <p>
-            <FaRegCompass />
-          </p>
-          <p>
-            <FaRegHeart />
-          </p>
-          <div className="user-img__header">
-            <img src={myProfileImg} alt="" />
+  // 글쓰기 모달창 제어
+  const onWriteClick = () => {
+    setWriteModalOn(true);
+  };
+  return (
+    <>
+      <div className="header">
+        <div className="header-area">
+          <div>
+            <h1>Eatstagram</h1>
+          </div>
+
+          <div className="header-area__icons">
+            <p>
+              <FaHome />
+            </p>
+            <p>
+              <FaRegPaperPlane />
+            </p>
+            <p onClick={onWriteClick}>
+              <FaRegPlusSquare />
+            </p>
+            <p>
+              <FaRegCompass />
+            </p>
+            <p>
+              <FaRegHeart />
+            </p>
+            <div className="user-img__header">
+              <img src={myProfileImg} alt="" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Modal isOpen={writeModalOn} setIsOpen={setWriteModalOn}>
+        <WriteModal />
+      </Modal>
+    </>
   );
 };
 
