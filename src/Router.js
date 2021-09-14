2a9c9ba0b7237d97779e5dbf75fd85a1892b4113
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-14 17:36:58 +0900

    router에서 체크기능 구현
    - 유저가 로그인 했는지 체크
    - 회원가입 토큰이 있는지 체크
    - redirect 기능 추가

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Route, Switch } from "react-router";
+import { Redirect, Route, Switch } from "react-router";
 import FindPassword from "./components/FindPassword";
 import Home from "./components/Home";
 import Join from "./components/Join";
@@ -9,13 +9,20 @@ import Main from "./components/Main";
 import WriteModal from "./components/WriteModal";
 
 const Router = () => {
+  const isLogin = localStorage.getItem("username");
+  const joinToken = sessionStorage.getItem("joinToken");
   return (
     <Switch>
-      <Route exact path="/" component={Login} />
+      {isLogin !== "undefined" && isLogin ? (
+        <Route exact path="/" component={Home} />
+      ) : (
+        <Route exact path="/" component={Login} />
+      )}
       <Route path="/Join" component={Join} />
       <Route path="/FindPassword" component={FindPassword} />
-      <Route path="/JoinEmail" component={JoinEmail} />
-      <Route path="/Home" component={Home} />
+      <Route path="/JoinEmail">
+        {joinToken != null && joinToken ? <JoinEmail /> : <Redirect to="/" />}
+      </Route>
       <Route path="/Main" component={Main} />
       <Route path="/WriteModal" component={WriteModal} />
     </Switch>
