b65353b10b30272a818c64b7fa95aa02c1ccaee3
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-18 05:11:53 +0900

    chat 추가 & header 공통 설정
    chat 추가 & header 공통 설정

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router";
+import Chat from "./components/Chat";
 import FindPassword from "./components/FindPassword";
+import Header from "./components/Header";
 import Home from "./components/Home";
 import Join from "./components/Join";
 import JoinEmail from "./components/JoinEmail";
@@ -13,14 +15,18 @@ const Router = () => {
   return (
     <Switch>
       {isLogin !== "undefined" && isLogin ? (
-        <Route exact path="/" component={Home} />
+        <>
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Route path="/WriteModal" component={WriteModal} />
+          <Route path="/Chat" component={Chat} />
+        </>
       ) : (
         <>
           <Route exact path="/" component={Login} />
           <Route path="/Join" component={Join} />
           <Route path="/FindPassword" component={FindPassword} />
           <Route path="/JoinEmail" component={JoinEmail} />
-          <Route path="/WriteModal" component={WriteModal} />
         </>
       )}
     </Switch>
