4960310cd80e688b9094e4f4d50ba7394c2d4117
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-19 01:57:11 +0900

    modal css 제어
    모달이 생겼을 때 스크롤 없애기 구현

--- src/Modal.js
+++ src/Modal.js
@@ -28,10 +28,19 @@ const Modal = ({ children, fade = false, isOpen, setIsOpen }, ref) => {
 
   useEffect(() => {
     if (isOpen) document.addEventListener("keydown", handleEscape, false);
+
     return () => {
       document.removeEventListener("keydown", handleEscape, false);
     };
   }, [handleEscape, isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.classList.add("modal-open");
+    } else {
+      document.body.classList.remove("modal-open");
+    }
+  }, [isOpen]);
 
   return createPortal(
     isOpen ? (
