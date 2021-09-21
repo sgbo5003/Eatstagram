fb98634f6b3d414489841d3a9131ca353d6eea5c
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-09-21 16:51:29 +0900

    axios default baseURL 변경
    axios default baseURL 변경
    localhost:8080 -> aws 주소로 변경

--- src/commonFunc/CommonObjFunctions.js
+++ src/commonFunc/CommonObjFunctions.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const executeQuery = ({ url, data, success, error, fail }) => {
-  axios.defaults.baseURL = "http://localhost:8080/";
+  // axios.defaults.baseURL = "http://localhost:8080/";
+  axios.defaults.baseURL =
+    "http://ec2-3-36-133-3.ap-northeast-2.compute.amazonaws.com:8080/";
 
   const params = new FormData();
   Object.keys(data).map((element) => {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import "../src/css/style.css";
 import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8080/";
+// axios.defaults.baseURL = "http://localhost:8080/";
+axios.defaults.baseURL =
+  "http://ec2-3-36-133-3.ap-northeast-2.compute.amazonaws.com:8080/";
 
 ReactDOM.render(
   <BrowserRouter>
