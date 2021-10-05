81aeb29eb5b87a4770fd43ad240c2fffbd00cd70
sgbo5003 <61876422+sgbo5003@users.noreply.github.com>
2021-10-06 00:30:06 +0900

    게시글 공유 관련
    - input 기능 구현

--- src/components/Home/ShareModal.js
+++ src/components/Home/ShareModal.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import storyProfileImg1 from "../../../public/images/명수스토리.jpg";
 import { FaTimes } from "react-icons/fa";
 
 const SharemModal = (props) => {
   const { setShareModalOn } = props;
+  const [inputText, setInputText] = useState("");
+  const [list, setList] = useState([]);
+  const [checked, setChecked] = useState("");
+
+  // input 입력값
+  const onInputTextHandler = (e) => {
+    setInputText(e.target.value);
+    if (e.target.value === "" || e.target.value === "") {
+      return;
+    } else {
+    }
+  };
+
+  // 보내기 버튼 제어
+  const onSubmit = () => {};
+  // 나가기 버튼 제어
   const onExitModalHandler = () => {
     setShareModalOn(false);
   };
@@ -16,33 +32,44 @@ const SharemModal = (props) => {
             <FaTimes onClick={onExitModalHandler} />
           </p>
           <h4>공유</h4>
-          <h2>보내기</h2>
+          <h2 onClick={onSubmit}>보내기</h2>
         </div>
         <div className="chat-start__middle">
           <h4>받는 사람 :</h4>
-          <div className="recommend-friend">
-            <div className="recommend-friend-btn">
-              <h4>whereyedo</h4>
-              <p>x</p>
-            </div>
-          </div>
-          <input type="text" placeholder="검색..." />
+          <input
+            type="text"
+            placeholder="검색..."
+            value={inputText}
+            onChange={onInputTextHandler}
+          />
         </div>
         <div className="chat-start__bottom">
           <div className="chat-recommend">
             <h4>추천</h4>
           </div>
           <div className="chat-recommend-area">
-            <div className="recommend-list">
-              <div className="recommend-user">
-                <img src={storyProfileImg1} alt="" />
-                <h4>whereyedo</h4>
-              </div>
-              <input type="radio" />
-            </div>
+            {list.map((data, idx) => {
+              return (
+                <div className="recommend-list" key={idx}>
+                  <div className="recommend-user">
+                    <img src={storyProfileImg1} alt="" />
+                    <div className="recommend-user-nameBox">
+                      <h4>
+                        {data.nickname === null ? "유저1" : data.nickname}
+                      </h4>
+                      <h6>{data.name}</h6>
+                    </div>
+                  </div>
+                  <input
+                    type="radio"
+                    checked={checked === data.username ? true : false}
+                    readOnly
+                  />
+                </div>
+              );
+            })}
           </div>
         </div>
-        <div></div>
       </div>
     </div>
   );
